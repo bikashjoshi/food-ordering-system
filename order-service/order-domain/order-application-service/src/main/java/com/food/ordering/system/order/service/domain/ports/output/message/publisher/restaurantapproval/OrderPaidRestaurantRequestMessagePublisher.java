c739619f876146c883b88d2ca2f382471767c44b
Bikash <bikashjoshi@gmail.com>
2023-05-30 23:19:23 -0400

    Adding Order Message Publisher and Consumer
    Signed-off-by: Bikash <bikashjoshi@gmail.com>

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
@@ -2,6 +2,7 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 
 import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 
-public interface OrderPaidRestaurantRequestMessagePublisher extends DomainEventPublisher<OrderEvent> {
+public interface OrderPaidRestaurantRequestMessagePublisher extends DomainEventPublisher<OrderPaidEvent> {
 }
