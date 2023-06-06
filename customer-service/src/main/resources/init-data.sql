bdedd5a0a17d30cf1d73e06c3eb8d920c06110ba
Bikash <bikashjoshi@gmail.com>
2023-06-05 22:55:02 -0400

    Payment Service Container Service
    Signed-off-by: Bikash <bikashjoshi@gmail.com>

--- customer-service/src/main/resources/init-data.sql
+++ customer-service/src/main/resources/init-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO customer.customers(id, username, first_name, last_name)
-       VALUES ('f046450b-87d3-49a2-a964-59f2db79976f', 'user_1', 'First', 'User');
+       VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user_1', 'First', 'User');
 
 INSERT INTO customer.customers(id, username, first_name, last_name)
-       VALUES ('198c44f6-9465-441c-abeb-15634c286847', 'user_2', 'Second', 'User');
+       VALUES ('d215b5f8-0249-4dc5-89a3-51fd148cfb42', 'user_2', 'Second', 'User');
