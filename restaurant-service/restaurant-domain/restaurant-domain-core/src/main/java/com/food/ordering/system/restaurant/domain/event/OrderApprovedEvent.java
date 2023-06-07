bed2ab4add621222e4add52e2995be0c8c2af8d4
Bikash <bikashjoshi@gmail.com>
2023-06-06 23:04:24 -0400

    Restaurant Service Domain Module
    Signed-off-by: Bikash <bikashjoshi@gmail.com>

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/domain/event/OrderApprovedEvent.java
@@ -0,0 +1,22 @@
+package com.food.ordering.system.restaurant.domain.event;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.restaurant.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderApprovedEvent extends OrderApprovalEvent {
+    private final DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher;
+
+    public OrderApprovedEvent(OrderApproval orderApproval, RestaurantId restaurantId, List<String> failureMessage, ZonedDateTime createdAt, DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher) {
+        super(orderApproval, restaurantId, failureMessage, createdAt);
+        this.orderApprovedEventDomainEventPublisher = orderApprovedEventDomainEventPublisher;
+    }
+
+    @Override
+    public void fire() {
+        orderApprovedEventDomainEventPublisher.publish(this);
+    }
+}
